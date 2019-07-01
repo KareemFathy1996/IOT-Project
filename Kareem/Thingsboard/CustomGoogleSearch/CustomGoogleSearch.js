f5f53dbadba13c8bb6dfccf3f8e745536af40b68
KareemFathy1996 <karemfathy1996@yahoo.com>
2019-07-01 19:52:20 +0200

    Delete CustomGoogleSearch.js

--- Kareem/Thingsboard/CustomGoogleSearch/CustomGoogleSearch.js
+++
@@ -1,6 +0,0 @@
-self.onInit = function() {
-    
-    var script = document.createElement("script")
-    script.src = "https://cse.google.com/cse.js?cx="+self.ctx.settings.SearchId
-    document.getElementsByTagName("body")[0].appendChild(script)
-}
